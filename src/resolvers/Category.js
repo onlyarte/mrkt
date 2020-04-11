2e0860ca0c393c9cc3eba66f2351ce2497064bdd
Ruslan Purii <ruslanpurii@gmail.com>
2020-04-11 10:20:26 +0200

    Filter, sort and paginate Category Items

--- src/resolvers/Category.js
+++ src/resolvers/Category.js
@@ -3,7 +3,15 @@ const parent = (parent, args, context, info) => {
 }
 
 const items = (parent, args, context, info) => {
-  return context.prisma.category({ id: parent.id }).items()
+  const { filter: where = {}, skip, first, orderBy } = args
+  const items = context.prisma
+    .category({ id: parent.id })
+    .items({ where, skip, first, orderBy })
+  const count = context.prisma
+    .itemsConnection({ where: { ...where, category: { id: parent.id } } })
+    .aggregate()
+    .count()
+  return { items, count }
 }
 
 module.exports = { parent, items }
